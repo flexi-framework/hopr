6a1057d89ae25f8b065e4a9a6d3f2b83819c6734
Thomas Bolemann <thomas.bolemann@iag.uni-stuttgart.de>
2017-08-23 14:33:27 +0200

    Introduce inner element stretching for stretched standard cartesian meshes. Parameter IneerElementStretch Default=F, if set True and BoundaryOrder>2 is used, elements are also stretched inside. Mostly used if stretched meshes are postdeformed, since Jacobians are then smoother...

--- src/mesh/mesh_vars.f90
+++ src/mesh/mesh_vars.f90
@@ -253,6 +253,7 @@ LOGICAL                        :: useCurveds             ! switch .TRUE.= we wan
 LOGICAL                        :: rebuildCurveds         ! switch .TRUE.= if curveds are already present in the mesh, delete them
                                                          ! and rebuild them using our methods
 LOGICAL                        :: meshIsAlreadyCurved    ! flag: mesh is already curved (GMSH, HDF5, block CGNS)
+LOGICAL                        :: InnerElemStretch       ! for cartmeshes, apply stretching also to inner element nodes
 !-----------------------------------------------------------------------------------------------------------------------------------
 ! CURVE GRID GENERATOR
 !-----------------------------------------------------------------------------------------------------------------------------------
