d3080994f48a03b4ee96f5a3a9f40913b42be52a
Florian Hindenlang <hindenlang@ipp.mpg.de>
2017-08-30 11:03:38 +0200

    postdeform 2.5d sinus and cos(1.5*pi*x) to have an unsymmetric deformation.

--- src/mesh/mesh_postdeform.f90
+++ src/mesh/mesh_postdeform.f90
@@ -687,18 +687,26 @@ CASE(21)!Laval nozzle
     xout(3)=x(3)
     X_out(:,i)=xout(:)
   END DO !i=1,nTotal
-CASE(30) ! sin
+CASE(30) ! sin [-1;1]^3
   x_out(1,:) = x_in(1,:)+ 0.1*SIN(Pi*x_in(1,:))*SIN(Pi*x_in(2,:))
   x_out(2,:) = x_in(2,:)+ 0.1*SIN(Pi*x_in(1,:))*SIN(Pi*x_in(2,:))
   x_out(3,:) = x_in(3,:)+ 0.1*SIN(Pi*x_in(1,:))*SIN(Pi*x_in(2,:))
-CASE(31) ! sin
+CASE(31) ! sin [-1;1]^3
   x_out(1,:) = x_in(1,:)+ 0.1*SIN(Pi*x_in(1,:))*SIN(Pi*x_in(2,:))*SIN(Pi*x_in(3,:))
   x_out(2,:) = x_in(2,:)+ 0.1*SIN(Pi*x_in(1,:))*SIN(Pi*x_in(2,:))*SIN(Pi*x_in(3,:))
   x_out(3,:) = x_in(3,:)+ 0.1*SIN(Pi*x_in(1,:))*SIN(Pi*x_in(2,:))*SIN(Pi*x_in(3,:))
-CASE(32) ! sin
+CASE(32) ! sin [-1;1]^3
   x_out(1,:) = x_in(1,:)+ 0.1*SIN(Pi*x_in(1,:))
   x_out(2,:) = x_in(2,:)+ 0.1*SIN(Pi*x_in(1,:))
   x_out(3,:) = x_in(3,:)+ 0.1*SIN(Pi*x_in(1,:))
+CASE(33) ! sin 2D [-1;1]^2
+  x_out(1,:) = x_in(1,:)+ 0.1*SIN(Pi*x_in(1,:))*SIN(Pi*x_in(2,:))
+  x_out(2,:) = x_in(2,:)+ 0.1*SIN(Pi*x_in(1,:))*SIN(Pi*x_in(2,:))
+  x_out(3,:) = x_in(3,:)
+CASE(34) ! cos3D (1.5Pi) [-1;1]^3
+  x_out(1,:) = x_in(1,:)+ 0.1*COS(1.5*Pi*x_in(1,:))*COS(1.5*Pi*x_in(2,:))*COS(1.5*Pi*x_in(3,:))
+  x_out(2,:) = x_in(2,:)+ 0.1*COS(1.5*Pi*x_in(1,:))*COS(1.5*Pi*x_in(2,:))*COS(1.5*Pi*x_in(3,:))
+  x_out(3,:) = x_in(3,:)+ 0.1*COS(1.5*Pi*x_in(1,:))*COS(1.5*Pi*x_in(2,:))*COS(1.5*Pi*x_in(3,:))
 END SELECT
 
 END SUBROUTINE PostDeformFunc
